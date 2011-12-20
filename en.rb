45789d514a2427a25ea852bf42260decff1ccb12
Chris Root <chrisr1386@gmail.com>
2011-12-20 12:53:37 -0500

    initial commit

---
+++ en.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+  :'en' =>
+  {
+    :hello => "What's crackin?",
+
+    :welcome => {
+      :index => {
+        :header => "Welcome to Edison Nation",
+        :title => "Welcome To Edison Nation Vietnam!",
+        :first_box => {
+          :p_1 => "<em>Edison Nation Vietnam</em> is a partnership between Edison Nation and Michael Diep, the first Vietnamese innovator to successfully introduce a product into the U.S. market.",
+          :p_2 => "Michael’s Emery Cat success has inspired the Vietnamese community in the U.S. and his native Vietnam.",
+          :p_3 => "Edison Nation Vietnam welcomes Vietnamese innovators into this safe and trusted community, offering resources and opportunities that might otherwise be limited. It is our mission to give those individuals with minds for ingenuity an opportunity to succeed."
+        }, # first_box
+        :middle_box => {
+          :button => "Sign Up For Free",
+          :idea_to_shelf => "We are an idea-to-shelf consumer product developer with extensive retail and manufacturing relationships. Share your creative genius with us and we will give your ideas the resources they need to grow.",
+          :featured_everywhere => "Edison Nation has been featured just about everywhere! ",
+          :my_idea => "\"That Was My Idea. I Wish I Had Done Something With It!\342\200\235"
+        }, # middle_box
+        :second_box => {
+          :title => "Commitment",
+          :quote => "“I have a lot to be thankful for as an inventor.  I have been fortunate to work with Edison Nation and to see firsthand their commitment to the development and commercialization of my innovation, Emery Cat. <em>Edison Nation</em> has the resources that were necessary to launch my innovation on television and into the retail market. Edison Nation is a company that innovators can trust with their dreams.\342\200\235"
+        } # second_box
+      } # index
+    } # welcome
+
+  } # en
+} # *
---
+++ vi.rb
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+  :'vi' =>
+  {
+    :hello => "What's crackin in VN?",
+    
+    :welcome => {
+      :index => {
+        :header => 'Welcome to Edison Nation VN',
+        :title => "Chào Mừng Bạn Đến Với Edison Nation Việt Nam!",
+        :first_box => {
+          :p_1 => "<em>Edison Nation Việt Nam</em> là một công ty hợp tác giữa Edison Nation và Michael Diệp, nhà sáng tạo Việt Nam đầu tiên đã giới thiệu thành công một sản phẩm vào thị trường Hoa Kỳ.",
+          :p_2 => "Emery Cat của Michael đã truyền cảm hứng cho cộng đồng người Việt ở Mỹ và tại Việt Nam.",
+          :p_3 => "Edison Nation Việt Nam chào đón những nhà sáng tạo Việt Nam tham gia cộng đồng đầy an toàn và tin tưởng hỗ trợ những nguồn lực và cơ hội về mặt nào đó có thể là chưa đầy đủ. Sứ mệnh của chúng tôi là trao cơ hội thành công cho những cá nhân có tài năng, khéo léo.",
+        },
+        :middle_box => {
+          :button => "Đăng ký miễn ph\303\255",
+          :idea_to_shelf => "Chúng tôi là nhà phát triển sản phẩm tiêu dùng từ-ý-tưởng-đến-kệ-hàng có hợp tác rộng rãi với các nhà sản xuất và nhà bán lẻ. Chia sẻ những ý tưởng sáng tạo của bạn và chúng tôi sẽ cung cấp nguồn lực giúp biến ý tưởng đó thành hiện thực.",
+          :featured_everywhere => "Edison Nation được đưa tin khắp nơi!",
+          :my_idea => "\"Đó là ý tưởng của tôi. Ước gì tôi có thể biến nó thành hiện thực!\""
+        },
+        :second_box => {
+          :title => "Cam kết",
+          :quote => "\"Tôi rất biết ơn để trở thành một nhà sáng tạo. Tôi đã may mắn được làm việc với Edison Nation và tận mắt thấy sự tận tụy của họ để phát triển và thương mại hóa sáng kiến của tôi, Emery Cat. <em>Edison Nation</em> có những nguồn lực cần thiết để giới thiệu sáng kiến của tôi trên truyền hình và vào thị trường bán lẻ. Edison Nation là người đồng hành mà các nhà sáng tạo có thể tin tưởng với những giấc mơ của họ.\""
+        }
+      }
+    }
+  }
+}
